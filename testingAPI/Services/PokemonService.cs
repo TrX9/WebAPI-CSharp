c2a63706712fa2e134d356628e1d22daca1e89c8
Tarek <TS@t>
2021-06-22 03:06:00 +0300

    Created PokemonService class and Edited Homecontroller

---
+++ testingAPI/Services/PokemonService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Web.Mvc;
+using testingAPI.Models;
+using CsvHelper;
+using System.Text.RegularExpressions;
+
+namespace testingAPI.Services
+{
+    public static class PokemonService
+    {
+        static List<Pokemon> Pokemons { get; }
+        static PokemonService()
+        {
+            string fileName = "pokemon.csv";
+            string path = Path.Combine(Environment.CurrentDirectory, @"Data\", fileName);
+            using (var streamReader = new StreamReader(path))
+            {
+                using (var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture))
+                {
+                    Pokemons = csvReader.GetRecords<Pokemon>().ToList();
+                    foreach (var Pokemon in Pokemons)
+                    {
+                        if (Pokemon.Legendary == true || Pokemon.Type_1 == "Ghost" || Pokemon.Type_2 == "Ghost") { Pokemon.Name = "ToDelete"; }
+                        if (Pokemon.Type_1 == "Steel" || Pokemon.Type_2 == "Steel") { Pokemon.HP *= 2; }
+                        if (Pokemon.Type_1 == "Fire" || Pokemon.Type_2 == "Fire") { Pokemon.Attack = (int)(Pokemon.Attack - Math.Floor(Pokemon.Attack * 0.1)); }
+                        if ((Pokemon.Type_1 == "Bug" || Pokemon.Type_1 == "Flying") && (Pokemon.Type_2 == "Bug" || Pokemon.Type_2 == "Flying")) { Pokemon.Speed = (int)(Pokemon.Speed + Math.Floor(Pokemon.Speed * 0.1)); }
+                        if (Regex.IsMatch(Pokemon.Name[0].ToString(), "G"))
+                        {
+                            int length = (Pokemon.Name.Length) - 1;
+                            Pokemon.Defense += (5 * length);
+                        }
+                    }
+                    Pokemons.RemoveAll(p => p.Name == "ToDelete");
+                }
+
+            }
+        }
+
+        public static List<Pokemon> GetAll()
+        {
+            return Pokemons;
+        }
+
+        public static Pokemon Get(int id)
+        {
+            return Pokemons.FirstOrDefault(p => p.Id == id);
+        }
+    }
+}
